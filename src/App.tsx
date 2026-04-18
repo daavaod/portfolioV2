1e26c6d79c48a72182fe84854de21dedb5df1c7e
Danijel Avramov <xiilxvi@Danijels-MacBook-Air.local>
2026-04-18 21:59:48 +0200

    lint check should pass; project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { ThemeProvider } from "@/components/theme/theme-provider";
+// router
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+// const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: {
+    // queryClient,
+  },
+  // defaultNotFoundComponent: NotFoundContent,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 function App() {
-  return <>CLEAN SLATE</>;
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="portfolioV2-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
